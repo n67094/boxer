db4a6c56031250ba34ef4b89289a5338efe7255a
nsix <n67094@proton.me>
2026-05-11 11:19:26 +0200

    chore: fix git

--- docs/docgen/README.md
+++
@@ -1,9 +0,0 @@
-# Docgen
-
-Generate documentation to markdown from C header files.
-
-## Usage
-
-```bash
-node docgen -o output.md header.h
-```
--- docs/docgen/lib/c.js
+++
@@ -1,9 +0,0 @@
-exports.applyToGenerator = function(g) {
-  g.addCodePattern(/\(/, (t) => {
-    t.header = "🔹 " + t.code;
-  });
-
-  g.addCodePattern(/^[^\(]*$/, (t) => {
-    t.header = "🔧 " + t.code;
-  });
-};
